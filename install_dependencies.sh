8fe3450c914a48e838feac37dc4ac6f3b6a79162
Rahul <rahul98anand@gmail.com>
2021-01-05 13:16:48 -0800

    Add files via upload

---
+++ install_dependencies.sh
@@ -0,0 +1,5 @@
+npm install
+npm install --save axios react-chartjs-2 react-countup classnames
+npm install --save @material-ui/core
+npm install --save chart.js
+npm install react-bootstrap bootstrap
